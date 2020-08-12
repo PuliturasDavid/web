83c13454c7adeddbfdef6d75a763926364a52448
Williams <williams.ing.pq@gmail.com>
2020-08-12 13:02:34 -0400

    navegacion modificada

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 document.querySelector(".boton-barras").addEventListener("click", ()=>{
     document.querySelector(".listas").classList.toggle("show");
 });
+
+ScrollReveal().reveal('.ShowCase');
+ScrollReveal().reveal('.cards', {delay: 500});
+ScrollReveal().reveal('.div-art-uno', {delay: 500});
+ScrollReveal().reveal('.SeccionDos', {delay: 500});
+ScrollReveal().reveal('.articuloDos', {delay: 500});
